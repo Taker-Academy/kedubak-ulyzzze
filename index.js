dff8a5a06f4c24e09015b447e49f454591c27d56
Ulysse Couchoud <ulysse.couchoud@epitech.eu>
2024-04-01 17:29:03 +0200

    [ADD] - connect with mongodb

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
-const app = require('express')();
 const PORT = 8080;
+var express = require('express');
+var server = express();
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://kanoisecouchoud:EqBTC2z6ZDHEMf2g@keduback.omperqd.mongodb.net/";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-app.listen(
-    PORT,
-    () => console.log(`working on http://localhost:${PORT}`)
-)
+async function run() {
+    try {
+        // Se connecter au serveur MongoDB
+        await client.connect();
 
-app.get('/tshirt', (req, res) => {
-    res.status(200).send({
-        tshirt: 'yellow',
-        size: 'large'
-    })
+        // Sélectionner la base de données
+        const database = client.db("KEDUBACK");
+
+        // Votre code ici pour interagir avec la base de données...
+
+    } finally {
+        // Assurez-vous de fermer la connexion lorsque vous avez fini
+        await client.close();
+    }
+}
+
+run().catch(console.dir);
+
+server.get('/', function (req, res) {
+    res.setHeader('Content-Type', 'text/html');
+    res.status(200).send('<h1>Bonjour sur mon serveur !</h1>');
 });
+
+server.listen(PORT, function() {
+    console.log(`working on http://localhost:${PORT}`)
+});
+
+//mongodb+srv://kanoisecouchoud:<password>@keduback.omperqd.mongodb.net/
