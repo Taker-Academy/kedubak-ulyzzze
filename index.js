57f10646d81e3f5d50cae24d7a17f4049658ecb6
Ulysse Couchoud <ulysse.couchoud@epitech.eu>
2024-04-03 20:51:56 +0200

    [FIX] - connect backend with docker

--- index.js
+++ index.js
@@ -5,15 +5,40 @@ var jwt = require('jsonwebtoken');
 // const { MongoClient } = require('mongodb');
 // const client = new MongoClient(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 
-server.get('/user', function (req, res) {
-    res.json({
-        text: 'my api'
-    });
-    res.setHeader('Content-Type', 'text/html');
-    res.status(200).send('<h1>Bonjour sur mon serveur !</h1>');
+// async function run() {
+//     await client.connect();
+//     console.log('Connection Ok');
+//     // const db = client.db('myTask');
+//     // const collection = db.collection('doc');
+//     // const insertStuff = await collection.insertMany([{a : 1}, {b : 2}, {c : 3}])
+//     // console.log(`Documents insérés : ${insertStuff}`);
+//     return 'done!';
+// }
+
+// run()
+//   .then(console.log)
+//   .catch(console.error)
+//   .finally(() => client.close())
+
+server.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
 });
 
-server.get('/user/login', function( req, res) {
+server.options('/post', function(req, res) {
+    res.sendStatus(200);
+})
+
+server.post('/post', function(req, res) {
+    res.send('POST request to /post');
+})
+
+server.get('/auth', function (req, res) {
+});
+
+server.get('/auth/login', function(req, res) {
     const user = { id: 3};
     const token = jwt.sign({ user }, 'my_secret_key');
     res.json({
@@ -21,13 +46,12 @@ server.get('/user/login', function( req, res) {
     });
 });
 
-server.get('/user/protection', ensureToken, function( req, res) {
+server.get('/auth/protection', ensureToken, function( req, res) {
     jwt.verify(req.token, 'my_secret_key', function(err, data) {
         if(err) {
             res.sendStatus(403);
         } else {
             res.json({
-                text: 'this is a protection',
                 data: data
             });
         }
